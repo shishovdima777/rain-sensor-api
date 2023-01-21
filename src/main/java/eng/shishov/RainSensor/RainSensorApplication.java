fbb84986bf0b4f23bfc23c4ab401d74897223e7e
Dima <shishovdima777@gmail.com>
2023-01-21 10:56:52 +0300

    initial commit

---
+++ src/main/java/eng/shishov/RainSensor/RainSensorApplication.java
@@ -0,0 +1,13 @@
+package eng.shishov.RainSensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eng/shishov/RainSensor/RainSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package eng.shishov.RainSensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
